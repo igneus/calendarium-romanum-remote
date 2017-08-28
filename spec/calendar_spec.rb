a122ffa976b70e71524f5c46bc484258fe8bbc9c
Jakub Pavlík <severus@post.cz>
2017-08-28 22:52:26 +0200

    test a whole year

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,24 +1,23 @@
 require_relative 'spec_helper'
 
 describe CalendariumRomanum::Remote::Calendar do
+  CR = CalendariumRomanum
+
   describe 'API-compatibility with Calendar'
 
   describe 'returns the same data as Calendar' do
     let(:year) { 2016 }
 
     # classical calendar
-    let(:sanctorale) { CalendariumRomanum::Data::GENERAL_ROMAN_ENGLISH.load }
-    let(:calendar) { CalendariumRomanum::Calendar.new(year, sanctorale) }
+    let(:sanctorale) { CR::Data::GENERAL_ROMAN_ENGLISH.load }
+    let(:calendar) { CR::Calendar.new(year, sanctorale) }
 
     # remote calendar with the same settings
     let(:uri) { REMOTE_CALENDAR_URI }
     let(:remote) { described_class.new(year, uri) }
 
-    dates = [
-      Date.new(2016, 12, 24),
-    ]
-
-    dates.each do |date|
+    days = (CR::Temporale::Dates.first_advent_sunday(2016) ... CR::Temporale::Dates.first_advent_sunday(2017))
+      days.each do |date|
       it date do
         c = calendar.day date
         r = remote.day date
